92c8f4106f203fae37a5937080e7ecc0dd7231cd
Coderchachaji <shailendraguptaa48@gmail.com>
2025-12-19 10:59:01 +0530

    Update Flask app for Vercel deployment
    Removed app.run() for Vercel deployment.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Loads index.html from /templates
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# No need for app.run() when deploying to Vercel
